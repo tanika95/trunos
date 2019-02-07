431910ca082b836e6e54855bfed0fcca4ba88be2
Tatiana Ermakova <t.ermakova@securitycode.ru>
2019-02-07 16:34:48 +0300

    Верное имя для LinkInfo

--- afdx/BandwidthInfo.hh
+++ afdx/BandwidthInfo.hh
@@ -4,13 +4,13 @@
 #include <stdint.h>
 #include "VlConfig.hh"
 
-typedef std::pair<uint32_t, uint32_t> Link;
+typedef std::pair<uint32_t, uint32_t> LinkInfo;
 
 class BandwidthInfo {
 public:
-	BandwidthInfo(const std::map<Link, double> &banwidth);
+	BandwidthInfo(const std::map<LinkInfo, double> &banwidth);
 
 	double getBanwidth(uint32_t sender, uint32_t receiver) const;
 private:
-	std::map<Link, double> banwidth;
+	std::map<LinkInfo, double> banwidth;
 };
